cd4b5fc0f5e4a2d524937ff521fdf02bdaf085af
akash <akash.agrawal445@gmail.com>
2023-10-27 16:50:36 +0530

    changes done in the pagination

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,8 +3,47 @@ const User = require("../schema/user.schema");
 module.exports.getUsersWithPostCount = async (req, res) => {
   try {
     //TODO: Implement this API
+    let { page, skip } = req.query;
+    page = page ? parseInt(page) : 1;
+    skip = skip ? parseInt(skip) : 1;
 
-    res.status(200).json({});
+    let userData = await User.aggregate([
+      {
+        $lookup: {
+          from: "posts",
+          let: { userId: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [{ $eq: ["$userId", "$$userId"] }],
+                },
+              },
+            },
+          ],
+          as: "user_data",
+        },
+      },
+
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          post: { $size: "$user_data" }
+        },
+      },
+      {
+        $sort: { _id: 1 },
+      },
+      {
+        $skip: skip,
+      },
+      {
+        $limit: 5,
+      },
+    ]);
+
+    res.status(200).json({ data: userData });
   } catch (error) {
     res.send({ error: error.message });
   }
